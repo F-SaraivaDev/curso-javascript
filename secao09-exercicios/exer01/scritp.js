0558ef7ecbdbb15d01a180617dace2ad177bf993
Fernando de Souza Saraiva - PrestServ <fernando.saraiva.prestserv@petrobras.com.br>
2023-12-10 21:56:55 -0300

    Exercicios secao 9

--- secao09-exercicios/exer01/scritp.js
+++ secao09-exercicios/exer01/scritp.js
@@ -1,5 +1,5 @@
 let frutas = ["Banana", "Maça", "Abacaxi", "Laranja", "Amora"]
 
-console.log(frutas[1])
+console.log(frutas[0])
+console.log(frutas[2])
 console.log(frutas[3])
-console.log(frutas[4])
